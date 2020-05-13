f8f6f3ff9c4dd79b0d68ca2864d419f2cca0b9e6
= <edid2204@hotmail.com>
2020-05-13 13:56:52 -0500

    Subiendo cambios de envio Post

--- app/src/main/java/com/pit/appcoronavirus/MainActivity.java
+++ app/src/main/java/com/pit/appcoronavirus/MainActivity.java
@@ -10,9 +10,16 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
+import com.android.volley.RequestQueue;
 import com.android.volley.Response;
+import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -52,11 +59,40 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
+                ejecutarServicio("http://192.168.1.46:8080/");
+
             }
         });
     }
 
     //Metodo que envia las peticiones al server url: ruta del webservice
+    private void ejecutarServicio(String URL){
 
+        StringRequest sr=new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Toast.makeText(getApplicationContext(),"OPERACION EXITOSA",Toast.LENGTH_SHORT).show();
+            }
+        }, new Response.ErrorListener(){
+            @Override
+            public void onErrorResponse(VolleyError error){
+                Toast.makeText(getApplicationContext(),error.toString(),Toast.LENGTH_SHORT).show();
+            }
+
+        }){
+            protected Map<String,String>getParams() throws AuthFailureError{
+                Map<String,String> parametros=new HashMap<String,String>();
+                parametros.put("NumCelular",numcel.getText().toString());
+                parametros.put("Nacionaidad",spinac.toString());
+                parametros.put("TipoDocumento",spitipo.toString());
+                parametros.put("NumDocumento",numdoc.getText().toString());
+                return parametros;
+            }
+        };
+
+        RequestQueue rq= Volley.newRequestQueue(this);
+        rq.add(sr);
+
+    }
 
 }
