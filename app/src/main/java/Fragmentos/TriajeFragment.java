eee63988a74b3e89d7372866b62fbe1a96a0b2ef
= <edid2204@hotmail.com>
2020-05-23 00:13:40 -0500

    Llamar a activity Regciudadano desde el boton RegTriaje del TriajeFragment

--- app/src/main/java/Fragmentos/TriajeFragment.java
+++ app/src/main/java/Fragmentos/TriajeFragment.java
@@ -1,6 +1,7 @@
 package Fragmentos;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -12,6 +13,7 @@ import android.widget.Button;
 import android.widget.CheckBox;
 
 import com.pit.appcoronavirus.R;
+import com.pit.appcoronavirus.Regciudadano;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -28,10 +30,23 @@ public class TriajeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_triaje, container, false);
+        // Obtener la vista
+        View v= inflater.inflate(R.layout.fragment_triaje, container, false);
 
+        //Encontrar el boton del fragment
+        Button btnTriaje=(Button) v.findViewById(R.id.btnRegTriaje);
 
+        //Generar el evento click listener
+        btnTriaje.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getActivity(), Regciudadano.class);
+                startActivity(intent);
+            }
+        });
 
+        //Retorna la vista
+        return v;
 
     }
 
